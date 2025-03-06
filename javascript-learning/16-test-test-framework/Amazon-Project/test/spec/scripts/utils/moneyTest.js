a708c8d115be618b7819dc7f9c1f60ca430ddd3f
C_11nJxx <c_11njxxsmailbox@sina.com>
2025-03-06 15:31:08 +0800

    Day12(AM) (Lesson16-Add test to convertMoney)

---
+++ javascript-learning/16-test-test-framework/Amazon-Project/test/spec/scripts/utils/moneyTest.js
@@ -0,0 +1,12 @@
+import {convertMoney} from '../../../../scripts/utils/money.js'
+describe('Test suite: convertMoney', () => {
+    it('Basic test',() => {
+        expect(convertMoney(4096)).toEqual(40.96);
+    });
+    it('0 test',() => {
+        expect(convertMoney(0)).toEqual(0.00);
+    });
+    it('float and round test',() => {
+        expect(convertMoney(409.6)).toEqual(4.10);
+    });
+});
