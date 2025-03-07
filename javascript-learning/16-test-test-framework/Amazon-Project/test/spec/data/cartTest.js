89814cfaa0637ae1102c9004a55272d70b6ef323
C_11nJxx <c_11njxxsmailbox@sina.com>
2025-03-07 10:52:18 +0800

    Day13(AM) (Lesson16-Exercises-16k)

--- javascript-learning/16-test-test-framework/Amazon-Project/test/spec/data/cartTest.js
+++ javascript-learning/16-test-test-framework/Amazon-Project/test/spec/data/cartTest.js
@@ -1,5 +1,4 @@
-import { cart, loadFromStorage, addToCart, removeFromCart } from "../../../data/cart.js";
-import { products } from "../../../data/products.js";
+import { cart, loadFromStorage, addToCart, removeFromCart, updateDeliveryOptions } from "../../../data/cart.js";
 
 describe('test suite: addToCart', () => {
 
@@ -103,5 +102,35 @@ describe('test suite: removeFromCart', () => {
             quantity: 2,
             deliveryOptionId: '3'
         }]));
+    });
+});
+
+describe('test suite: updateDeliveryOption',() => {
+    const productId1 = 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6';
+    beforeEach(() => {
+        spyOn(localStorage,'setItem');
+        spyOn(localStorage,'getItem').and.callFake(() => {
+            return JSON.stringify([{
+                productId: productId1,
+                quantity: 1,
+                deliveryOptionId: '1'
+            }]);
+        });
+        loadFromStorage();
+    });
+    it('updates the delivery option',() => {
+        updateDeliveryOptions(productId1,'3');
+        expect(cart[0].deliveryOptionId).toEqual('3');
+        expect(cart.length).toEqual(1);
+        expect(cart[0].productId).toEqual(productId1);
+        expect(cart[0].quantity).toEqual(1);
+        expect(localStorage.setItem).toHaveBeenCalledTimes(1);
+        expect(localStorage.setItem).toHaveBeenCalledWith('cart',JSON.stringify([
+            {
+                productId: productId1,
+                quantity: 1,
+                deliveryOptionId: '3'
+            }
+        ]))
     });
 })
