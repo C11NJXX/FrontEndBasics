ef963fe9365d06ee9f377632eca6abab9d8d5738
C_11nJxx <c_11njxxsmailbox@sina.com>
2025-03-07 09:26:05 +0800

    Day13(AM) (Lesson16-Exercises-16e)

--- javascript-learning/16-test-test-framework/Amazon-Project/test/spec/data/cartTest.js
+++ javascript-learning/16-test-test-framework/Amazon-Project/test/spec/data/cartTest.js
@@ -3,8 +3,11 @@ import { products } from "../../../data/products.js";
 
 describe('test suite: addToCart', () => {
 
-    it('adds a product into an empty cart', () => {
+    beforeEach(() => {
         spyOn(localStorage, 'setItem');
+    });
+
+    it('adds a product into an empty cart', () => {
         spyOn(localStorage, 'getItem').and.callFake(() => {
             return JSON.stringify([]);
         });
@@ -23,7 +26,6 @@ describe('test suite: addToCart', () => {
     });
 
     it('adds an existing product into cart', () => {
-        spyOn(localStorage, 'setItem');
         spyOn(localStorage, 'getItem').and.callFake(() => {
             return JSON.stringify([{
                 productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
