1f41660514a75cf781f3cd9ba80518aed14bcb3a
C_11nJxx <c_11njxxsmailbox@sina.com>
2025-03-08 10:58:48 +0800

    Day14(AM) (Lesson17-Exercise-17c)

--- javascript-learning/17-object-oriented-programming/Amazon-Project/data/car.js
+++ javascript-learning/17-object-oriented-programming/Amazon-Project/data/car.js
@@ -1,12 +1,24 @@
 class Car {
     brand;
     model;
+    speed;
     constructor(brand,model) {
         this.brand = brand;
         this.model = model;
+        this.speed = 0;
     }
     displayInfo() {
-        console.log(`${this.brand} ${this.model}`);
+        console.log(`${this.brand} ${this.model}, Speed: ${this.speed} km/h`);
+    }
+
+    go() {
+        if(this.speed >= 0 && this.speed <=195) 
+        this.speed += 5;
+    }
+
+    break() {
+        if(this.speed >= 5 && this.speed <=200)
+        this.speed -= 5
     }
 };
 
@@ -19,3 +31,14 @@ console.log(car2);
 car1.displayInfo();
 car2.displayInfo();
 
+for(let i = 0 ; i < 100 ; i++ ) {
+    car1.go();
+}
+car1.break();
+
+car2.go();
+car2.break();
+car2.break();
+
+car1.displayInfo();
+car2.displayInfo();
