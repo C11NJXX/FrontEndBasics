79744c386db3e866a360b8ac230f5a503f25a59d
C_11nJxx <c_11njxxsmailbox@sina.com>
2025-03-08 11:06:51 +0800

    Day14(AM) (Lesson17-Exercise-17d)

--- javascript-learning/17-object-oriented-programming/Amazon-Project/data/car.js
+++ javascript-learning/17-object-oriented-programming/Amazon-Project/data/car.js
@@ -2,23 +2,38 @@ class Car {
     brand;
     model;
     speed;
+    isTrunkOpen;
     constructor(brand,model) {
         this.brand = brand;
         this.model = model;
         this.speed = 0;
+        this.isTrunkOpen = false;
     }
     displayInfo() {
-        console.log(`${this.brand} ${this.model}, Speed: ${this.speed} km/h`);
+        console.log(`${this.brand} ${this.model}, Speed: ${this.speed} km/h, Trunk is ${this.isTrunkOpen ? "opened" : "closed"}`);
     }
 
     go() {
-        if(this.speed >= 0 && this.speed <=195) 
-        this.speed += 5;
+        if(!this.isTrunkOpen) {
+            if(this.speed >= 0 && this.speed <=195) 
+            this.speed += 5;
+        }else {
+            console.log('Refuse to go, the trunk is opened');
+        }
     }
 
     break() {
         if(this.speed >= 5 && this.speed <=200)
         this.speed -= 5
+    }
+    openTrunk() {
+        if(this.speed === 0) {
+            this.isTrunkOpen = true;
+        }else {
+            console.log(`Refused to open, The car is running at speed: ${this.speed}`)
+        }
+    }
+    closeTrunk() {
     }
 };
 
@@ -42,3 +57,16 @@ car2.break();
 
 car1.displayInfo();
 car2.displayInfo();
+
+car1.openTrunk();
+car1.go();
+car1.displayInfo();
+console.log("Break!!!!!!!!!");
+for(let i = 0; i < 100; i++ ) {
+    car1.break();
+};
+car1.displayInfo();
+car1.openTrunk();
+car1.displayInfo();
+car1.go();
+car1.displayInfo();
